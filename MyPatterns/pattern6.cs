974daaaa70feb0610cb46e4ed5d5a5aa12839835
rajesh@cloudmojo.tech <rajesh@cloudmojo.tech>
2026-03-12 16:57:45 +0530

    added pattern

---
+++ MyPatterns/pattern6.cs
@@ -0,0 +1,15 @@
+public class pattern6
+{
+    public static void Main(string[] args)
+    {
+        int n=5;
+        for(int i = 0; i < n; i++)
+        {
+            for(int j = 1; j <= n - i; j++)
+            {
+                Console.Write(j);
+            }
+            Console.WriteLine();
+        }
+    }
+}
